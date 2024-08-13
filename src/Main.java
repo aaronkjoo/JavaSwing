9a8d0fcf1c717f68dce9ee29b4c097ff46af83f6
Aaron Joo <ajoo8@gatech.edu>
2024-08-12 19:02:17 -0700

    bro code

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(420,420);
+        frame.setTitle("JFrame title goes here");
+    }
+}
